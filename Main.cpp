8dc7d9a9651d3b7c812700c85496ad60a7d5e367
AshtonB08 <93924688+AshtonB08@users.noreply.github.com>
2022-02-10 14:07:30 +0000

    Add project files.

---
+++ Main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <ctime>
+
+using namespace std;
+
+const int rows = 10;
+const int elements = 10;
+int matrix[rows][elements];
+
+int main()
+{
+
+
+	system("pause");
+	return 0;
+}
